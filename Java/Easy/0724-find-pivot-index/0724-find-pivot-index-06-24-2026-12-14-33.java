97bbe1e5f31cafccd91af4d3c136486d6b3ad117
Harsh Mishra <mishra30harsh@gmail.com>
2026-06-24 12:14:32 +0530

    0724-find-pivot-index Array .java

---
+++ Java/Easy/0724-find-pivot-index/0724-find-pivot-index-06-24-2026-12-14-33.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total=0,left=0,right=0;
+        for(int i=0;i<nums.length;i++)total=total+nums[i];
+        for(int i=0;i<nums.length;i++){
+         
+            
+            right=total-left-nums[i];
+            if(left==right)return i;
+             left=left+nums[i];
+              
+            
+        }
+       return -1;
+    }
+}
