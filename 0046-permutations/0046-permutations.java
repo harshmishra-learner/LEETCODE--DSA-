bc8d925f82234ed2d6bb2fb0ebcb4036688751c7
Harsh Mishra <mishra30harsh@gmail.com>
2026-07-14 18:11:53 +0530

    0046-permutations Array .java

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -4,34 +4,37 @@ import java.util.List;
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        // Tracking array to know which elements are currently inside our path
-        boolean[] visited = new boolean[nums.length];
-        
-        backtrack(nums, new ArrayList<>(), visited, result);
+        solve(0, nums, result);
         return result;
     }
 
-    private void backtrack(int[] nums, List<Integer> currentList, boolean[] visited, List<List<Integer>> result) {
-        // Base Case: If the temporary list matches the size of nums, we found a permutation
-        if (currentList.size() == nums.length) {
-            result.add(new ArrayList<>(currentList)); // Deep copy the list
+    private void solve(int index, int[] nums, List<List<Integer>> result) {
+        // Base Case: If index reaches the end, a full permutation is ready
+        if (index == nums.length) {
+            List<Integer> currentPermutation = new ArrayList<>();
+            for (int num : nums) {
+                currentPermutation.add(num);
+            }
+            result.add(currentPermutation);
             return;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            // If the element at index i is already used, skip it
-            if (visited[i]) continue;
+        // Loop through all available elements to place at the current 'index' position
+        for (int i = index; i < nums.length; i++) {
+            // 1. Swap the element at 'i' into our target 'index' position
+            swap(nums, index, i);
 
-            // 1. Take the element
-            visited[i] = true;
-            currentList.add(nums[i]);
+            // 2. Recurse to solve permutations for the remaining positions
+            solve(index + 1, nums, result);
 
-            // 2. Move down the decision tree
-            backtrack(nums, currentList, visited, result);
-
-            // 3. Backtrack (Undo the choice)
-            currentList.remove(currentList.size() - 1);
-            visited[i] = false;
+            // 3. Backtrack: Swap back to clean up state for the next loops
+            swap(nums, index, i);
         }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
